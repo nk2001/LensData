87021bd2f945c6c66d69a7dc1e0e876b9597f1d2
nk2001 <kadoor@yahoo.com>
2021-02-25 21:44:35 -0800

    Updates

--- app.py
+++ app.py
@@ -1,22 +1,24 @@
 import streamlit as st
 
-st.title('Contact Lens Fitting App')
-st.header("Type of Contact Lens")
-st.image('https://www.pexels.com/photo/person-holding-silver-wedding-band-5843337/', use_column_width=True)
-
-st.subheader("This App would suggest the type of lens for an individual based on the following information:")
+st.title('Contact Lens Fitting Application')
+#st.header("Type of Contact Lens")
+st.subheader("This App would suggest the type of lens for an individual")
 
 st.text("Dataset used from UCI repository: https://archive.ics.uci.edu/ml/datasets/Lenses")
-st.text("Age group of the patient: (1)Young (2)Pre-presbyopic (3)Presbyopic")
-st.text("Spectacle prescription:  (1)myope (2)hypermetrope")
-st.text("Astigmatic: (1)No, (2)Yes")
-st.text("Tear production rate: (1) Reduced, (2) Normal")
+st.image('https://www.lensdirect.com/blog/wp-content/uploads/2019/02/Comparing-Contacts.jpg', use_column_width=True)
+st.text("image:https://www.lensdirect.com/blog/wp-content/uploads/2019/02/Comparing-Contacts.jpg")
+
+
+#st.text("Age group of the patient: (1)Young (2)Pre-presbyopic (3)Presbyopic")
+#st.text("Spectacle prescription:  (1)myope (2)hypermetrope")
+#st.text("Astigmatic: (1)No, (2)Yes")
+#st.text("Tear production rate: (1) Reduced, (2) Normal")
 
 
 
 
 
-st.subheader('Please fill in the details on the left sidebar and click on the button below!')
+st.subheader('Please fill in the details on the left sidebar and click on the button below')
 
 
 Agegrp = st.sidebar.radio("Patient Age Group", options=["Young", "Pre-presbyopic", "Presbyopic"])
